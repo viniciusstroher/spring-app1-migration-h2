d4f53dac529acb7ddfd25001777435aceaab76ab
veni <viniciusstroher@gmail.com>
2020-06-21 23:18:33 -0300

    spring h2 setup migration

---
+++ src/main/resources/db/migration/V1.0.1__startupdb.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE test_data (
+ name VARCHAR(25) NOT NULL PRIMARY KEY
+);
+
+/*
+Multi-line
+comment
+*/
+
+-- Sql-style comment
+
+-- Placeholder
+INSERT INTO test_data (name) VALUES ('Mr. T');
+INSERT INTO test_data (name) VALUES ('Mr. Semicolon');
---
+++ src/test/java/br/veni/App1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.veni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
